b6fc2c5065bb9e1a984df78bff0757f021a01c52
Yi Chiang and russ vetsper <yi+russ@>
2016-07-19 14:18:06 -0700

    add first test for geting array

--- Objects/Coin.cs
+++ Objects/Coin.cs
@@ -1,9 +1,37 @@
+using System;
+using System.Collections.Generic;
+
 namespace CoinList.Objects
 {
   public class Coin
   {
     string[] coinName= new string[4]{"Quarter","Dime","Nickel","Penny"};
     double[] coinValue= new double[4]{0.25,0.1,0.05,0.01};
+    private double _inputMoney;
+    private int[] _result;
+    public Coin(double amount)
+    {
+      _inputMoney=amount;
+    _result= new int[4]{0,0,0,0};
+    }
+
+    public double GetInputMoney()
+    {
+      return _inputMoney;
+    }
+    public void SetInputMoney(double newAmount)
+    {
+      _inputMoney=newAmount;
+    }
+    public int[] GetResultArray()
+    {
+      return _result;
+    }
+    public void SetResultArray(int[] newArray)
+    {
+      _result=newArray;
+    }
+
 
     }
 
--- Tests/CoinTest.cs
+++ Tests/CoinTest.cs
@@ -1,11 +1,20 @@
 using Xunit;
 using System.Collections.Generic;
-
+using System;
 
 namespace CoinList.Objects
 {
   public class CoinTest
   {
-
+    [Fact]
+    public void Test1_Get_Result_array_returnCombination()
+    {
+      int[] newArray= new int[4]{3,4,3,4};
+      Coin myCoin = new Coin(0.34);
+      myCoin.SetResultArray(newArray);
+      Console.WriteLine(myCoin.GetResultArray());
+      Console.WriteLine(newArray);
+      Assert.Equal(newArray,myCoin.GetResultArray());
+    }
   }
 }
