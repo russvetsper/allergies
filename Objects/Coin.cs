f06a77d82538104ad870219e8098101e8f1b563d
Yi Chiang and russ vetsper <yi+russ@>
2016-07-19 15:25:57 -0700

    have floor problems

--- Objects/Coin.cs
+++ Objects/Coin.cs
@@ -5,14 +5,13 @@ namespace CoinList.Objects
 {
   public class Coin
   {
-    string[] coinName= new string[4]{"Quarter","Dime","Nickel","Penny"};
-    double[] coinValue= new double[4]{0.25,0.1,0.05,0.01};
+
     private double _inputMoney;
-    private int[] _result;
+    private double[] _result;
     public Coin(double amount)
     {
       _inputMoney=amount;
-    _result= new int[4]{0,0,0,0};
+    _result= new double[4]{0,0,0,0};
     }
 
     public double GetInputMoney()
@@ -23,15 +22,41 @@ namespace CoinList.Objects
     {
       _inputMoney=newAmount;
     }
-    public int[] GetResultArray()
+    public double[] GetResultArray()
     {
       return _result;
     }
-    public void SetResultArray(int[] newArray)
+    public void SetResultArray(double[] newArray)
     {
       _result=newArray;
     }
 
+    public double[] TestCombination()
+    {
+      string[] coinName= new string[4]{"Quarter","Dime","Nickel","Penny"};
+      double[] coinValue= new double[4]{0.25,0.10,0.05,0.01};
+      double money=this.GetInputMoney();
+      double[] resultCombination=new double[4]{0,0,0,0};
+
+      for (int i=0; i<4; i++) {
+        if(money/coinValue[i]>=1)
+        {
+          Console.WriteLine(coinValue[i]+" divided into "+money);
+
+          double remainder=money/coinValue[i];
+          double index= Math.Floor( remainder);
+          Console.WriteLine("number of " + coinName[i] + " in " + remainder + " is " + index);
+          Console.WriteLine("after floor is "+Math.Floor( remainder));
+
+          resultCombination[i]=index;
+          money=money-(coinValue[i]*index);
+
+        }
+      }
+
+
+      return resultCombination;
+    }
 
     }
 
--- Tests/CoinTest.cs
+++ Tests/CoinTest.cs
@@ -6,15 +6,62 @@ namespace CoinList.Objects
 {
   public class CoinTest
   {
+    // [Fact]
+    // public void Test1_Get_Result_array_returnArray()
+    // {
+    //   int[] newArray= new int[4]{3,4,3,4};
+    //   Coin myCoin = new Coin(0.34);
+    //   myCoin.SetResultArray(newArray);
+    //   Console.WriteLine(myCoin.GetResultArray());
+    //   Console.WriteLine(newArray);
+    //   Assert.Equal(newArray,myCoin.GetResultArray());
+    // }
+    // [Fact]
+    // public void Test1_oneQuarter_returnCombination()
+    // {
+    //   Coin myCoin = new Coin(0.25);
+    //   int[] resultArray= new int[4]{1,0,0,0};
+    //   Assert.Equal(resultArray,myCoin.TestCombination());
+    // }
+    //
+    // [Fact]
+    // public void Test2_ThreeQuarter_returnCombination()
+    // {
+    //   Coin myCoin = new Coin(0.75);
+    //   int[] resultArray= new int[4]{3,0,0,0};
+    //   Assert.Equal(resultArray,myCoin.TestCombination());
+    // }
+
+    // [Fact]
+    // public void Test3_OneDimes_returnCombination()
+    // {
+    //   Coin myCoin = new Coin(0.10);
+    //   int[] resultArray= new int[4]{0,1,0,0};
+    //   Assert.Equal(resultArray,myCoin.TestCombination());
+    // }
+    //
+    // [Fact]
+    // public void Test4_TwoDimes_returnCombination()
+    // {
+    //   Coin myCoin = new Coin(0.20);
+    //   int[] resultArray= new int[4]{0,2,0,0};
+    //   Assert.Equal(resultArray,myCoin.TestCombination());
+    // }
+
     [Fact]
-    public void Test1_Get_Result_array_returnCombination()
+    public void Test5_Complex_returnCombination()
     {
-      int[] newArray= new int[4]{3,4,3,4};
-      Coin myCoin = new Coin(0.34);
-      myCoin.SetResultArray(newArray);
-      Console.WriteLine(myCoin.GetResultArray());
-      Console.WriteLine(newArray);
-      Assert.Equal(newArray,myCoin.GetResultArray());
+      Coin myCoin = new Coin(0.95);
+      double[] resultArray= new double[4]{3,2,0,0};
+      Assert.Equal(resultArray,myCoin.TestCombination());
     }
+    //
+    // [Fact]
+    // public void Test6_TwoDimes_returnCombination()
+    // {
+    //   Coin myCoin = new Coin(0.34);
+    //   int[] resultArray= new int[4]{1,0,1,4};
+    //   Assert.Equal(resultArray,myCoin.TestCombination());
+    // }
   }
 }
